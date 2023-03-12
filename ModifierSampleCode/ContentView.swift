c6f10012fae9faf6894bc580aa7db4ff30c41f6a
David Hu <davidhu.sg66@gmail.com>
2023-03-12 16:44:39 +0800

    git init

---
+++ ModifierSampleCode/ContentView.swift
@@ -0,0 +1,91 @@
+//
+//  ContentView.swift
+//  ModifierSampleCode
+//
+//  Created by David Hu on 12/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var isLoading: Bool = false
+    
+    var body: some View {
+        Button {
+            // mock a network request or any loading action
+            isLoading = true
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                isLoading = false
+            }
+        } label: {
+            VStack {
+                Image(systemName: "arrow.triangle.2.circlepath.circle")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("Mock Loading")
+            }
+        }
+        .padding()
+        .modifier(ActivityIndicatorModifier(isLoading: isLoading))
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct ActivityIndicator: UIViewRepresentable {
+    @Binding var isAnimating: Bool
+    let style: UIActivityIndicatorView.Style
+
+    func makeUIView(context: UIViewRepresentableContext<ActivityIndicator>) -> UIActivityIndicatorView {
+        return UIActivityIndicatorView(style: style)
+    }
+
+    func updateUIView(_ uiView: UIActivityIndicatorView, context: UIViewRepresentableContext<ActivityIndicator>) {
+        isAnimating ? uiView.startAnimating() : uiView.stopAnimating()
+    }
+}
+
+struct ActivityIndicatorModifier: AnimatableModifier {
+    var isLoading: Bool
+
+    init(isLoading: Bool, color: Color = .primary, lineWidth: CGFloat = 3) {
+        self.isLoading = isLoading
+    }
+
+    var animatableData: Bool {
+        get { isLoading }
+        set { isLoading = newValue }
+    }
+
+    func body(content: Content) -> some View {
+        ZStack {
+            if isLoading {
+                GeometryReader { geometry in
+                    ZStack(alignment: .center) {
+                        content
+                            .disabled(self.isLoading)
+                            .blur(radius: self.isLoading ? 3 : 0)
+
+                        VStack {
+                            Text("Now Loading")
+                            ActivityIndicator(isAnimating: .constant(true), style: .large)
+                        }
+                        .frame(width: geometry.size.width / 2,
+                               height: geometry.size.height / 5)
+                        .background(Color.secondary.colorInvert())
+                        .foregroundColor(Color.primary)
+                        .cornerRadius(20)
+                        .opacity(self.isLoading ? 1 : 0)
+                        .position(x: geometry.frame(in: .local).midX, y: geometry.frame(in: .local).midY)
+                    }
+                }
+            } else {
+                content
+            }
+        }
+    }
+}
---
+++ ModifierSampleCode/ModifierSampleCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModifierSampleCodeApp.swift
+//  ModifierSampleCode
+//
+//  Created by David Hu on 12/3/23.
+//
+
+import SwiftUI
+
+@main
+struct ModifierSampleCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
